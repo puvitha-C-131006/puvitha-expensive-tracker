b79b81327c7fab8bb91576692d733a74bc9a0bbc
[dyad] <git@dyad.sh>
2025-11-07 18:48:29 +0530

    [dyad] Removed the dedicated header component from the home page. - wrote 1 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,10 @@
 import { Link } from "react-router-dom";
 import { GradientButton } from "@/components/GradientButton";
-import { HomeHeader } from "@/components/HomeHeader";
 import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-      <HomeHeader />
-      
+    <div className="min-h-screen flex flex-col justify-between">
       <main className="flex-1 flex items-center justify-center p-4">
         <div className="text-center p-8 bg-card rounded-lg shadow-xl page-fade-in">
           <h1 className="text-5xl font-extrabold mb-4 text-card-foreground">
